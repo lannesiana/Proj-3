336793eefbc422ec689414eaa2b99e2d8700c3fc
fatpenguin67 <fatpenguin68@yahoo.com>
2013-08-06 20:16:28 -0700

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,1 +1,24 @@
 Public class Block{
+    private int upLeftRow;
+    private int upLeftColumn;
+    private int botRightRow;
+    private int botRightColumn;
+    private final String[] directions = new String["up", "down", "left", "right"];
+    
+    public Block(int upLeftRow, int upLeftColumn, int botRightRow, int botRightColumn){
+        this.upLeftRow = upLeftRow;
+        this.upLeftColumn = upLeftColumn;
+        this.botRightRow = botRightRow;
+        this.botRightColumn = botRightColumn;
+        
+        // do we want to put blocks into a hash? need hash code?
+    }
+    
+    public void moveBlock(String direction) throws Exception{ //gotta check if possible to move, a "canMove" method?
+        if (!directions.contains(direction))
+          throw (new Exception e{});
+        if direction.equals("up"){
+          this.upLeftRow
+        }
+      
+    }
