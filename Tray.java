87fb6f2ee669ef30a95623b1c80cbd76d09609a4
lannesiana <lynnanni@yahoo.com>
2013-08-06 20:39:43 -0700

    Update Tray.java

--- Tray.java
+++ Tray.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 
-
-
-
-
 public class Tray{
-    final private int rowsTotal;
+	final private int rowsTotal;
 	final private int colsTotal;
 	final private ArrayList<Block> blocks;
 	final private int hash;
     public int[][] board;
+    final private int[][] layout;
     
 
     public Tray(int rows, int columns, ArrayList<Block> blocks,
@@ -25,7 +22,14 @@ public class Tray{
         
     }
     private int calculateDaHashCode(){
-    	
+    	int prime = 47;
+    	int result = 1;
+    	for (int r = 0; r < rowsTotal; r++) {
+			for (int c = 0; c < colsTotal; c++) {
+				result = prime * result + layout[r][c];
+			}
+    }
+    	return result; 
     }
 
     public int hashCode(){
