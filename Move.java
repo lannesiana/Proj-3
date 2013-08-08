0b63a5abc1a7833ebb3473e183872f0d269175ee
fatpenguin67 <fatpenguin68@yahoo.com>
2013-08-08 13:38:59 -0700

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,16 +1,31 @@
 
 public class Move {
+	private String moveString = "";
 	private Block block;
-	public enum Direction{
-		up, down, left, right
-	}
 	private Direction dir;
 	
 	public Move(Block block, Direction dir) {
 		this.block = block;
 		this.dir = dir;
+		String destination = getNewCoords(dir);
+		String source = block.getULR() + " " + block.getULC() + " ";
+		moveString = source + destination;
+
 	}
 	
+	//returns the destination of the upper corner of the block
+	private String getNewCoords(Direction dir){
+		String s = "";
+		if (dir == Direction.up)
+			s = (block.getULR() - 1) + " " + block.getULC();
+		if (dir == Direction.down)
+			s = (block.getULR() + 1) + " " + block.getULC();
+		if (dir == Direction.left)
+			s = (block.getULR() + " " + (block.getULC() - 1));
+		if (dir == Direction.right)
+			s = (block.getULR() + " " + (block.getULC() + 1));
+		return s;
+	}
 	public Direction getDirection(){			
 		return dir;
 	}
@@ -18,8 +33,10 @@ public class Move {
 		return block + " moves " + dir;
 	}
 
-	public String toString(){ //returns a move according to specs
-
+	//returns a move according to specs in the form
+	// 1 1 0 1: first 2 nums are upLeft of a block, latter 2 are destination coordinates of the upLeft corner of the block.
+	public String toString(){ 
+		return moveString;
 	}
 	
 	public Block getBlock() {
@@ -29,6 +46,10 @@ public class Move {
 		
 		//DO SOMETHING IN TRAY?
 
+	}
+	
+	public boolean equals(){
+		//is this even necessary?
 	}
 	
 }
