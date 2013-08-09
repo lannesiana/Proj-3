e78d674a47ea5d736d963c0225a9af8c0455fee0
fatpenguin67 <fatpenguin68@yahoo.com>
2013-08-09 00:24:40 -0700

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 public class Solver {
+  private Tray initialTray;
+	private List<Block> goalBlock;
+	private Queue<Tray> trayQueue = new LinkedList<Tray>();
+	private Set<Integer> visitedTrays = new HashSet<Integer>();
+	private boolean debug
+
 
 }
+
